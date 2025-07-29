0e202f8045579fe0215a76614edb60b8bca5b7f3
dell <dell@DESKTOP-MNCF89K>
2025-07-29 08:25:41 +0530

    Git push2

---
+++ src/test/java/testcase/Gitpushh2.java
@@ -0,0 +1,14 @@
+package testcase;
+
+public class Gitpushh2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		System.out.println("git pushh2 ");
+	}
+
+}
